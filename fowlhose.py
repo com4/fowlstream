530e1f33b31cf6a96cdd310fcd5984beb6ced2f8
jason <jason@zzq.org>
2020-04-22 20:35:14 -0600

    add cli options for fowlhose

--- fowlhose.py
+++ fowlhose.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env -S python -u
 #-*- fill-column: 79 -*-
 """Fowlhose - Stream tweets based on filter rules.
 
@@ -15,14 +15,24 @@
   - ``FOWLBIRD_LOG_FORMAT`` - If you want a different log format
   - ``TWITTER_ACCESS_TOKEN`` - API Key from app page
   - ``TWITTER_SECRET_KEY`` - API Secret Key from app page
+
+* Usage
+  ./fowlhose.py set-rule doggos "puppy has:images"
+  ./fowlhose.py set-rule kitties "kittie has:images"
+  ./fowlhose.py list-rules
+  ./fowlhose.py stream > rainy_day_pics.json
 """
 import asyncio
 from base64 import b64encode
+from collections import OrderedDict
+import json
 import logging
 import os
 import sys
+from typing import Any, Dict, IO, List, Optional
 from urllib.parse import quote as urlquote
 
+__description__ = "Filter and follow the Twitter Stream"
 __version__ = "0.1"
 
 class ColorizedFormatter(logging.Formatter):
@@ -77,6 +87,91 @@ STREAM_URL = "{}/labs/1/tweets/stream/filter".format(BASE_URL)
 RULES_URL = "{}/labs/1/tweets/stream/filter/rules".format(BASE_URL)
 
 
+def print_ascii_table(
+        data: List[List[Any]],
+        headers: Optional[List[str]] = None,
+        stream: IO = sys.stdout):
+    """Prints an ASCII table from data in a dictionary.
+
+    .. note::
+
+       Order will matter. Take care to make sure the order of your column data
+       doesn't change from row to row (or header and color).
+
+    Args:
+        data: Each sub-list in ``data`` will be a row in the table. Each
+            element in the sub-list will be a column.
+        header: Each element in the list will be used as a header.
+        stream: The stream to write the table to.
+    Raises:
+        ValueError: header length and column count mismatch or column count
+            differences.
+    """
+    num_columns = None
+    col_max_lengths = {}
+
+    # ###
+    # Calculate the max length of the column
+    for i, row in enumerate(data):
+        if num_columns is None:
+            num_columns = len(row)
+            if headers and len(headers) != num_columns:
+                raise ValueError(
+                    "Missing header values: (row[{}]){} != {}(headers)".format(
+                        i, len(row), len(headers)))
+
+        if num_columns != len(row):
+            raise ValueError(
+                "Column length mismatch. All columns should be the same "
+                "length. Calculated {} row[{}] has {}".format(
+                    num_columns, i, len(row)))
+
+        for j, column in enumerate(row):
+            col_len = len(column)
+            if j not in col_max_lengths:
+                col_max_lengths[j] = col_len
+                continue
+            elif col_max_lengths[j] < col_len:
+                col_max_lengths[j] = col_len
+
+    if headers:
+        # Are any of the headers longer than the
+        for i, header in enumerate(headers):
+            header_len = len(header)
+            if i not in col_max_lengths:
+                col_max_lengths[i] = header_len
+            elif col_max_lengths[i] < header_len:
+                col_max_lengths[i] = header_len
+
+    #   total of col_max_lengths
+    # + (the number of columns +1(for the last pipe and space)
+    #    * 2 (one for the pipe and space added)
+    # This will work if there is a leading and trailing space to "round" the
+    # corners
+    ruler_length = sum(col_max_lengths.values()) + ((len(col_max_lengths) + 1) * 2)
+
+    stream.write(" {}\n".format("-"*ruler_length))
+    if headers:
+        # start of the header row
+        stream.write("|")
+        for i, header in enumerate(headers):
+            stream.write(" {v:<{l}} |".format(v=header, l=col_max_lengths[i]))
+        stream.write("\n")
+        # Separator ruler
+        stream.write("|")
+        for i, header in enumerate(headers):
+            # Add 2 for the added space and pipe
+            stream.write("{v}|".format(v="="*(col_max_lengths[i] + 2)))
+        stream.write("\n")
+
+    for row in data:
+        stream.write("|")
+        for i, column in enumerate(row):
+            stream.write(" {v:<{l}} |".format(v=column, l=col_max_lengths[i]))
+        stream.write("\n")
+    stream.write(" {}\n".format("-"*ruler_length))
+
+
 async def _oauth_get_bearer_token(
         client: aiohttp.ClientSession, access_token: str,
         secret_token: str) -> str:
@@ -88,7 +183,7 @@ async def _oauth_get_bearer_token(
        doing before authentication.
 
     Args:
-        client: The HTTP client to make requests with
+        client: HTTP client to use for the request.
         access_token: Your app's access token from developer.twitter.com
         secret_token: Your app's secret token from developer.twitter.com
 
@@ -125,15 +220,55 @@ async def _oauth_get_bearer_token(
     return bearer_token
 
 
-async def get_filter_rules(client: aiohttp.ClientSession) -> dict:
-    ret = None
+async def list_filter_rules(client: aiohttp.ClientSession) -> List[Dict[str, str]]:
+    """Get all filter rules for this account.
+
+    Args:
+        client: HTTP client to use for the request.
+
+    Return:
+        A list of rules
+        [{'id': '<rule_id>', 'value': '<rule>', 'name': '<name>'}, ]
+    """
+    data = None  # Response from API
+    ret = []  # Return value
+
     async with client.get(RULES_URL) as response:
-        ret = await response.json()
-    return ret
+        logger.debug("GET {} -> {} {}".format(
+            RULES_URL, response.status, response.reason))
+        data = await response.json()
+        logger.debug("{}".format(data))
+
+    if data and "data" in data:
+        for rule in data["data"]:
+            ret.append({
+                "id": rule["id"],
+                "value": rule["value"],
+                "name": rule["tag"]})
+
+    return tuple(ret)
 
 
-async def set_filter_rule(client: aiohttp.ClientSession, name: str, rule: str):
-    # https://developer.twitter.com/en/docs/labs/filtered-stream/guides/search-queries
+async def add_filter_rule(
+        client: aiohttp.ClientSession,
+        name: str,
+        rule: str) -> bool:
+    """Create a filter rule.
+
+    .. seealso:
+
+       For the filter rule syntax, see Twitters documentation.
+       https://developer.twitter.com/en/docs/labs/filtered-stream/guides/search-queries
+
+    Args:
+        client: HTTP client to use for the request.
+        name: the name of the rule
+        rule: the filter to create
+
+    Return:
+        ``True`` if the rule creation was successful
+    """
+
     payload = {
         "add": [
             {"value": rule, "tag": name},
@@ -142,17 +277,40 @@ async def set_filter_rule(client: aiohttp.ClientSession, name: str, rule: str):
     async with client.post(RULES_URL, json=payload) as response:
         logger.debug("POST {} -> {} {}".format(
             RULES_URL, response.status, response.reason))
-        logger.debug(await response.json())
+        data = await response.json()
+        logger.debug(data)
+    return data["meta"]["summary"]["created"] == 1
 
 
-async def delete_all_filter_rules(client: aiohttp.ClientSession):
-    rules = await get_filter_rules(client)
+async def reset_filter_rules(client: aiohttp.ClientSession) -> bool:
+    """Remove all filters.
 
-    if "data" not in rules:
-        # No rules to delete
-        return
+    Args:
+        client: HTTP client to use for the request.
 
-    ids = list(d["id"] for d in rules["data"])
+    Returns:
+        ``True`` if _all_ rules were deleted. ``False`` if one or more rules
+        were not deleted.
+    """
+    rules = await list_filter_rules(client)
+
+    if not rules:
+        return True
+
+    ids = tuple(d["id"] for d in rules)
+    return await delete_filter_rules(client, ids)
+
+
+async def delete_filter_rules(client: aiohttp.ClientSession, ids: List[int]) -> bool:
+    """Remove a list of ids from the ruleset.
+
+    Args:
+        client: HTTP client to use for the request.
+        ids: A list of ids to remove
+    Returns:
+        ``True`` if _all_ rules were deleted. ``False`` if one or more rules
+        were not deleted.
+    """
     payload = {
         "delete": {
             "ids": ids,
@@ -161,6 +319,24 @@ async def delete_all_filter_rules(client: aiohttp.ClientSession):
     async with client.post(RULES_URL, json=payload) as response:
         logger.debug("POST {} -> {} {}".format(
             RULES_URL, response.status, response.reason))
+        data = await response.json()
+        logger.debug(data)
+
+    return data["meta"]["summary"]["not_deleted"] == 0\
+
+
+async def delete_filter_rule(client: aiohttp.ClientSession, id_: int) -> bool:
+    """Remove the provided id from the ruleset
+
+    Args:
+        client: HTTP client to use for the request.
+        id_: The rule id to remove
+    Returns:
+        ``True`` if _all_ rules were deleted. ``False`` if one or more rules
+        were not deleted.
+    """
+    ids = [id_, ]
+    return await delete_filter_rules(client, ids)
 
 
 async def create_client(access_token: str, secret_token: str) -> aiohttp.ClientSession:
@@ -179,7 +355,8 @@ async def create_client(access_token: str, secret_token: str) -> aiohttp.ClientS
 async def connect_stream(client: aiohttp.ClientSession):
     async with client.get(STREAM_URL) as response:
         async for tweet in response.content:
-            tweet = tweet.decode("utf-8")
+            tweet = tweet.decode("utf-8").strip()
+            if not tweet: continue
             yield tweet
 
 
@@ -193,13 +370,115 @@ async def stream_tweets(access_token: str, secret_token: str):
     await client.close()
 
 
-
-async def _main(loop):
+if __name__ == "__main__":
+    import argparse
 
     TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
     TWITTER_ACCESS_SECRET = os.getenv("TWITTER_ACCESS_SECRET")
 
     logger.setLevel(logging.DEBUG)
+
+    async def cmd_list_rules(args: argparse.Namespace):
+        try:
+            client = await create_client(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+            rules = await list_filter_rules(client)
+
+            if True:  # if human readable
+                header = ("id", "name", "value")
+                rows = tuple(
+                    [r[header[0]], r[header[1]], r[header[2]]] for r in rules)
+                print_ascii_table(rows, header)
+            else:  # if machine readable
+                sys.stdout.write("{}\n", json.dumps(rules))
+
+        finally:
+            await client.close()
+
+    # async def cmd_get_rule(args: argparse.Namespace):
+    #     try:
+    #         client = await create_client(
+    #             TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+    #         r = await add_filter_rule(client, args.name, args.value)
+    #     finally:
+    #         await client.close()
+
+    async def cmd_set_rule(args: argparse.Namespace):
+        try:
+            client = await create_client(
+                TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+            r = await add_filter_rule(client, args.name, args.value)
+        finally:
+            await client.close()
+
+        if r:
+            logger.info("Successfully added rule")
+        else:
+            logger.error("Unable to add rule")
+
+    async def cmd_reset_rules(args: argparse.Namespace):
+        try:
+            client = await create_client(
+                TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+            r = await reset_filter_rules(client)
+        finally:
+            await client.close()
+
+        if r:
+            logger.info("Successfully reset rules")
+        else:
+            logger.error("One or more rules were not removed during reset")
+
+
+    async def cmd_delete_rule(args: argparse.Namespace):
+        try:
+            client = await create_client(
+                TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+            r = await delete_filter_rule(client, args.id)
+        finally:
+            await client.close()
+
+        if r:
+            logger.info("Successfully removed rule {}".format(args.id))
+        else:
+            logger.error("Unable to remove rule {}".format(args.id))
+
+    async def cmd_stream(args: argparse.Namespace):
+        async for tweet in stream_tweets(
+                TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET):
+            sys.stdout.write("{}\n".format(tweet))
+
+    parser = argparse.ArgumentParser(description=__description__)
+    subparser = parser.add_subparsers(dest="command")
+
+    list_rules_parser = subparser.add_parser(
+        "list-rules", help="list your filter rules")
+    list_rules_parser.set_defaults(func=cmd_list_rules)
+
+    # get_rule_parser = subparser.add_parser(
+    #     "get-rule", help="get a filter rule by id")
+    # get_rule_parser.add_argument("id", type=int, help="rule id to retrieve")
+
+    set_rule_parser = subparser.add_parser(
+        "set-rule", help="define a new filter rule.")
+    set_rule_parser.add_argument(
+        "name", type=str, help="a name to identify this rule")
+    set_rule_parser.add_argument("value", type=str, help="the rule")
+    set_rule_parser.set_defaults(func=cmd_set_rule)
+
+    reset_rules_parser = subparser.add_parser(
+        "reset-rules", help="reset all filter rules")
+    reset_rules_parser.set_defaults(func=cmd_reset_rules)
+
+    delete_rule_parser = subparser.add_parser(
+        "delete-rule", help="delete a filter rule by id")
+    delete_rule_parser.add_argument("id", type=int, help="rule to delete")
+    delete_rule_parser.set_defaults(func=cmd_delete_rule)
+
+    stream_parser = subparser.add_parser(
+        "stream", help="follow the twitterverse with your filter rules")
+    stream_parser.set_defaults(func=cmd_stream)
+
+    args = parser.parse_args()
 
     sys.stderr.write("\n")
     sys.stderr.write(
@@ -208,31 +487,16 @@ async def _main(loop):
         "\u001b[38;5;33m _   \u001b[38;5;8m/\u001b[0m\n")
     sys.stderr.write(
         "\u001b[38;5;33m(\u001b[38;5;12m@\u001b[38;5;33m)"
-        "\u001b[38;5;3m<\u001b[0m     Fowlhose - Stream tweets based on "
-        "filter rules\n")
+        "\u001b[38;5;3m<\u001b[0m     Fowlhose - {}\n".format(__description__))
     sys.stderr.write(
         "\u001b[38;5;33m/-\\\u001b[0m      Version: {}\n\n".format(
             __version__))
 
-    logger.debug("FOWLBIRD_LOG_FORMAT: {}".format(LOG_FORMAT))
     logger.debug("TWITTER_ACCESS_TOKEN: xxxxx{}".format(
         TWITTER_ACCESS_TOKEN[-7:]))
     logger.debug("TWITTER_ACCESS_SECRET: xxxxx{}".format(
         TWITTER_ACCESS_SECRET[-7:]))
 
-    async for tweet in stream_tweets(
-            TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET):
-        sys.stdout.write("{}\n".format(tweet.strip()))
-
-    # client = await create_client(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
-    #
-    # await delete_all_filter_rules(client)
-    # await set_filter_rule(client, "covid-19", "covid-19")
-    # rules = await get_filter_rules(client)
-    # logger.debug("Rules: {}".format(rules))
-    #
-    # await client.close()
-
-if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(_main(loop))
+    loop.run_until_complete(args.func(args))
+    loop.close()
